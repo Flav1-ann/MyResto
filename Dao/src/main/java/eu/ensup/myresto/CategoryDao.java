b3624eeb9b6f36f48db38c2eedba93be310c91e3
flavien <flavien.annaix@gmail.com>
2021-12-10 11:47:05 +0100

    dao

--- Dao/src/main/java/eu/ensup/myresto/CategoryDao.java
+++ Dao/src/main/java/eu/ensup/myresto/CategoryDao.java
@@ -20,161 +20,36 @@ public class CategoryDao extends BaseDao implements ICategoryDao {
 
     @Override
     public List<Category> getAll() throws DaoException {
-        List<Category> allCategory = new ArrayList<>();
-
-        try {
-            connexion();
-            setPs(getCn().prepareStatement("SELECT * FROM category"));
-            setRs(getPs().executeQuery());
-
-            ResultSet res = getRs();
-            while (res.next()) {
-                Category category = new Category(res.getInt("id"), res.getString("name"), res.getString(IMAGE));
-                allCategory.add(category);
-            }
-
-            if (allCategory.isEmpty()) {
-                log.info("Aucune category disponible dans la base de donnée.");
-            }
-
-            disconnect();
-        } catch (SQLException e) {
-            log.error(e.getMessage());
-            throw new DaoException(UserDao.class.getName(), "getAll", e.getMessage(), ERR_ACCESS);
-        }
-
-        return allCategory;
+        return null;
     }
 
     @Override
     public Category getById(int idCategory) throws DaoException {
-        try {
-            connexion();
-            setPs(getCn().prepareStatement("SELECT * FROM category WHERE id = ?"));
-            getPs().setInt(1, idCategory);
-        } catch (SQLException e) {
-            throw new DaoException(CategoryDao.class.getName(), "get", e.getMessage(), ERR_ACCESS);
-        }
-
-        return getSubMethod();
+        return null;
     }
 
     @Override
     public Category getByName(String nameCategory) throws DaoException {
-        try {
-            connexion();
-            setPs(getCn().prepareStatement("SELECT * FROM category Where name = ?"));
-            getPs().setString(1, nameCategory);
-        } catch (SQLException e) {
-            throw new DaoException(CategoryDao.class.getName(), "get", e.getMessage(), ERR_ACCESS);
-        }
-        return getSubMethod();
+        return null;
     }
-
-    private Category getSubMethod() throws DaoException {
-        Category category = null;
-        try {
-            setRs(getPs().executeQuery());
-            ResultSet res = getRs();
-            String logInfo = "La categorie  n'est pas disponible dans la base de donnée.";
-            if (!res.next()) {
-                log.info(logInfo);
-            } else {
-                category = new Category(res.getInt("id"), res.getString("name"), res.getString(IMAGE));
-            }
-            disconnect();
-        } catch (SQLException e) {
-            log.error(e.getMessage());
-            throw new DaoException(CategoryDao.class.getName(), "get", e.getMessage(), "Une erreur s'est produite lors de la recherche d'une categorie");
-        }
-        return category;
-    }
-
 
     @Override
     public int create(Category category) throws DaoException {
-        try {
-            //Vérifie qu'il n'y a pas de double
-            String logInfo = String.format("La categorie %s existe déjà", category.getName());
-            if (getByName(category.getName()) == null) {
-                connexion();
-
-                setPs(getCn().prepareStatement("INSERT INTO category (name, image) VALUES ( ?, ? )"));
-
-                getPs().setString(1, category.getName());
-                getPs().setString(2, category.getImage());
-
-                setResult(getPs().executeUpdate());
-                disconnect();
-                return getResult();
-            } else {
-                log.error(logInfo);
-                throw new DaoException(CategoryDao.class.getName(), "create", null, "Cette categorie existe déjà");
-            }
-        } catch (SQLException e) {
-            log.error(e.getMessage());
-            throw new DaoException(CategoryDao.class.getName(), "create", e.getMessage(), "Une erreur s'est produite lors de la creation d'une categorie");
-        }
+        return 0;
     }
 
     @Override
     public int update(Category category) throws DaoException {
-        try {
-            connexion();
-
-            Category preCategory = getById(category.getId());
-
-            boolean haveUpdate = category.getName() != null && !category.getName().equals(preCategory.getName());
-            haveUpdate = haveUpdate || category.getImage() != null && category.getImage().equals(preCategory.getImage());
-            String logInfo = String.format("Cette categorie n'as pas été modifier %s", category.getName());
-            //Vérifie qu'il y a eu des modification
-            if (haveUpdate) {
-                setPs(getCn().prepareStatement("UPDATE category SET name = ?, image = ? WHERE id = ?"));
-
-                getPs().setString(1, category.getName());
-                getPs().setString(2, category.getImage());
-                getPs().setInt(3, category.getId());
-
-                setResult(getPs().executeUpdate());
-                disconnect();
-                return getResult();
-            } else {
-                log.error(logInfo);
-                return 1;
-            }
-        } catch (SQLException e) {
-            log.error(e.getMessage());
-            throw new DaoException(CategoryDao.class.getName(), "update", e.getMessage(), "Une erreur s'est produite lors de la mise a jour d'une categorie");
-        }
+        return 0;
     }
 
     @Override
     public int delete(Category category) throws DaoException {
-        if (category.getId() == null)
-            return delete(getByName(category.getName()).getId());
-        return delete(category.getId());
+        return 0;
     }
 
     @Override
     public int delete(int idCategory) throws DaoException {
-        try {
-            String logInfo = String.format("La categorie %d n'existe pas !", idCategory);
-            if (idCategory != -1 && getById(idCategory) != null) {
-                connexion();
-
-                setPs(getCn().prepareStatement("DELETE FROM category WHERE id = ?"));
-                getPs().setInt(1, idCategory);
-
-                setResult(getPs().executeUpdate());
-                disconnect();
-                return getResult();
-            } else {
-                log.info(logInfo);
-                throw new DaoException(CategoryDao.class.getName(), "delete", "La categorie " + idCategory + " n'existe pas !", "Cette categorie n'existe pas");
-            }
-        } catch (SQLException e) {
-            log.error(e.getMessage());
-            throw new DaoException(CategoryDao.class.getName(), "delete", e.getMessage(), "Une erreur s'est produite lors de la suppression d'une categorie");
-        }
+        return 0;
     }
 }
--- Dao/src/main/java/eu/ensup/myresto/OrderProductDao.java
+++ Dao/src/main/java/eu/ensup/myresto/OrderProductDao.java
@@ -13,154 +13,36 @@ public class OrderProductDao extends BaseDao implements IOrderProductDao {
 
     @Override
     public int createOrderProduct(OrderProduct orderProduct) throws DaoException {
-        try {
-            connexion();
-            int idOrder = new Random().nextInt(100000);
-            String sql = "INSERT INTO `order_product`(`id`, `id_User`, `date`, `status`) VALUES (?,?,?,?)";
-            setPs(getCn().prepareStatement(sql));
-            getPs().setLong(1, idOrder);
-            getPs().setInt(2,orderProduct.getIdUser());
-            getPs().setDate(3, orderProduct.getDateCreated());
-            getPs().setString(4,orderProduct.getStatus());
-            setResult(getPs().executeUpdate());
-            
-            for (int idProducts:orderProduct.getIdProduct()) {
-                String sqlListproducts = "INSERT INTO `listproducts`(`id_product`, `id_order`) VALUES (?,?)";
-                setPs(getCn().prepareStatement(sqlListproducts));
-                getPs().setInt(1, idProducts);
-                getPs().setInt(2,idOrder);
-                setResult(getPs().executeUpdate());
-            }
-        } catch (SQLException e) {
-            throw new DaoException(OrderProductDao.class.getName(), "createOrderProduct", e.getMessage(), "Une erreur s'est produite lors de la création de la commande");
-        }
-        return getResult();
+        return 0;
     }
 
     @Override
     public Set<OrderProduct> getAllOrderProductsForOneUser(int idUser) throws DaoException {
-        Set<OrderProduct> orderProducts = new HashSet<>();
-        try {
-            connexion();
-            String sql = "SELECT * FROM `order_product` where id_User = ?";
-            setPs(getCn().prepareStatement(sql));
-            getPs().setInt(1, idUser);
-            setRs(getPs().executeQuery());
-            ResultSet result = getRs();
-            while (result.next()) {
-                List<Integer> tabIdsProducts = new ArrayList<>();
-                String sql2 = "SELECT * FROM `listproducts` where id_order = ?";
-                setPs(getCn().prepareStatement(sql2));
-                getPs().setInt(1, result.getInt("id"));
-                setRs(getPs().executeQuery());
-                while (getRs().next()) {
-                    tabIdsProducts.add(getRs().getInt("id_product"));
-                }
-               orderProducts.add(new OrderProduct(result.getInt("id"), result.getInt("id_User") ,tabIdsProducts,result.getDate("date"), result.getString("status")));
-            }
-        } catch (SQLException e) {
-            throw new DaoException(OrderProductDao.class.getName(), "getAllOrderProducts", e.getMessage(), "Une erreur s'est produite lors de la récupération de tout les commande");
-        }
-        return orderProducts;
+        return null;
     }
 
     @Override
     public Set<OrderProduct> getAllOrderProduct() throws DaoException {
-        Set<OrderProduct> orderProducts = new HashSet<>();
-        try {
-            connexion();
-            String sql = "SELECT * FROM `order_product`";
-            setPs(getCn().prepareStatement(sql));
-            setRs(getPs().executeQuery());
-            ResultSet result = getRs();
-            while (result.next()) {
-                List<Integer> tabIdsProducts = new ArrayList<>();
-                String sql2 = "SELECT * FROM `listproducts` where id_order = ?";
-                setPs(getCn().prepareStatement(sql2));
-                getPs().setInt(1, result.getInt("id"));
-                setRs(getPs().executeQuery());
-                while (getRs().next()) {
-                    tabIdsProducts.add(getRs().getInt("id_product"));
-                }
-                orderProducts.add(new OrderProduct(result.getInt("id"), result.getInt("id_User") ,tabIdsProducts,result.getDate("date"), result.getString("status")));
-            }
-        } catch (SQLException e) {
-            throw new DaoException(OrderProductDao.class.getName(), "getAllOrderProducts", e.getMessage(), "Une erreur s'est produite lors de la récupération de tout les commande");
-        }
-        return orderProducts;
+        return null;
     }
 
     @Override
     public int updateOrderProduct(OrderProduct orderProduct) throws DaoException {
-        try {
-            connexion();
-            String sql = "UPDATE `order_product` SET `id_User`=?,`idProduct`=?,`date`=?,`status`=?, WHERE `id`= ?";
-            setPs(getCn().prepareStatement(sql));
-            getPs().setInt(1, orderProduct.getIdUser());
-            getPs().setDate(3, orderProduct.getDateCreated());
-            getPs().setString(4, orderProduct.getStatus());
-            getPs().setInt(5, orderProduct.getId());
-            setResult(getPs().executeUpdate());
-            disconnect();
-        } catch (SQLException e) {
-            throw new DaoException(OrderProductDao.class.getName(), "updateOrderProduct", e.getMessage(), "Une erreur s'est produite lors de la mise à jour de la commande");
-        }
-        return getResult();
+        return 0;
     }
 
     @Override
     public int updateOrderProductById(int id, String value) throws DaoException {
-        try {
-            connexion();
-            String sql = "UPDATE `order_product` SET `status`=? WHERE `id`= ?";
-            setPs(getCn().prepareStatement(sql));
-            getPs().setString(1, value);
-            getPs().setInt(2, id);
-            setResult(getPs().executeUpdate());
-            disconnect();
-        } catch (SQLException e) {
-            throw new DaoException(OrderProductDao.class.getName(), "updateOrderProductById", e.getMessage(), "Une erreur s'est produite lors de la mise à jour de la commande");
-        }
-        return getResult();
+        return 0;
     }
 
     @Override
     public int deleteOrderProduct(int idOrderProduct) throws DaoException {
-        try {
-            connexion();
-            String sql = "DELETE FROM `order_product` WHERE `id`=? ";
-            setPs(getCn().prepareStatement(sql));
-            getPs().setInt(1, idOrderProduct);
-            setResult(getPs().executeUpdate());
-        } catch (SQLException e) {
-            throw new DaoException(OrderProductDao.class.getName(), "deleteOrderProduct", e.getMessage(), "Une erreur s'est produite lors de la suppression de la commande");
-        }
-        return getResult();
+        return 0;
     }
 
     @Override
     public OrderProduct getOneOrderProduct(int idOrderProduct) throws DaoException {
-        try {
-            connexion();
-            String sql = "SELECT * FROM `order_product` where id = ?";
-            setPs(getCn().prepareStatement(sql));
-            getPs().setInt(1, idOrderProduct);
-            setRs(getPs().executeQuery());
-            ResultSet result = getRs();
-            while (result.next()) {
-                List<Integer> tabIdsProducts = new ArrayList<>();
-                String sql2 = "SELECT * FROM `listproducts` where id_order = ?";
-                setPs(getCn().prepareStatement(sql2));
-                getPs().setInt(1, idOrderProduct);
-                setRs(getPs().executeQuery());
-                while (getRs().next()) {
-                    tabIdsProducts.add(getRs().getInt("id_product"));
-                }
-                return new OrderProduct(result.getInt("id"), result.getInt("id_User") ,tabIdsProducts,result.getDate("date"), result.getString("status"));
-            }
-        } catch (SQLException e) {
-            throw new DaoException(OrderProductDao.class.getName(), "getAllOrderProducts", e.getMessage(), "Une erreur s'est produite lors de la récupération de tout les commande");
-        }
         return null;
     }
 }
